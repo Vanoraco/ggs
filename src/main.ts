2ec2b96489f560588372469fe604925b34247cab
Yordanos tibebu <yordanostibeu5@gmail.com>
2023-11-01 04:31:20 -0700

    Added About us page

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideRouter } from '@angular/router';
 import { AppModule } from './app/app.module';
+import routeConfig from './app/components/routes';
+import { AppComponent } from './app/app.component';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent,
+    {
+      providers: [
+        provideProtractorTestingSupport(),
+        provideRouter(routeConfig)
+      ]
+    }).catch(err => console.error(err));
+
