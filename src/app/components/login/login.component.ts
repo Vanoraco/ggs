e5f2a98ed01a0f65ac34773e589f37eac19a914c
pirate776 <kingzaljaguar@gmail.com>
2023-12-19 16:21:27 +0300

    login and register authentication added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm!: FormGroup
+  registerForm! : FormGroup
+     constructor(private fb: FormBuilder) {
+      
+     }
+
+     ngOnInit(): void {
+      this.loginForm = this.fb.group(
+        {
+          username: '',
+          password: ''
+        }
+      )
+      this.registerForm = this.fb.group(
+        {
+          regusername: '',
+          regpassword: '',
+          regconfirmPassword: '',
+          regemail: ''
+        }
+      )
+     } 
+  
+     get login() {
+      return this.loginForm.controls;
+    }
+
+    get register() {
+      return this.registerForm.controls
+    }
+
+    submitLogin() {
+     console.log({ user: this.login['username'].value, pass: this.login['password'].value}) 
+    }
+
+    submitRegister() {
+    console.log({ username: this.register['regusername'].value, regpass: this.register['regpassword'].value, regconfpas: this.register['regconfirmPassword'].value, regemail: this.register['regemail'].value})
+    }
 
 }
